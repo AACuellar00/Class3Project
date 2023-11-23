8d114080c1ed3e7f916afbca55cd286985f70d23
AdrianC <aacuellar14@gmail.com>
2023-11-23 09:52:54 -0800

    moved to src

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__, template_folder='frontend/templates', static_folder='frontend/static')
     app.config['SECRET_KEY'] = 'fhakjlssdfhaeunfa'
 
-    from website.frontend.views import views
+    from src.frontend.views import views
 
     app.register_blueprint(views, url_prefix='/')
     return app
