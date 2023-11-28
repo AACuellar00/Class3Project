64e82bf3ab1411c4d77b5dede904001586f12922
AdrianC <aacuellar14@gmail.com>
2023-11-28 00:49:40 -0800

    test

--- src/analyzer/__init__.py
+++ src/analyzer/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from .. import db
 
-db = SQLAlchemy()
+
 DB_NAME = "database.db"
 
 def create_app():
