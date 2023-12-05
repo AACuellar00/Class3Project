e3ac385412212172fc8b18c7ba81c3149755d8c0
AdrianC <aacuellar14@gmail.com>
2023-12-04 21:39:52 -0800

    .

--- src/app.py
+++ src/app.py
@@ -1,9 +1,6 @@
 from src import create_app
-from prometheus_flask_exporter import PrometheusMetrics
 
 app = create_app()
-metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'Application info', version='1.0.0')
 
 if __name__ == '__app__':
     app.run(debug=True)
